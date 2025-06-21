c00533ec00002dbb38fa442cd3144cd6af35ccfc
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 00:41:34 +0530

    Update Socket.js

--- backend/src/Utils/Socket.js
+++ backend/src/Utils/Socket.js
@@ -14,7 +14,7 @@ export function initializeSocket(server) {
     cors: {
       origin: process.env.CORS_ORIGIN,
       methods: ["GET", "POST"],
-      credentials: true,jects.vercel.app/"
+      credentials: true,
     },
   });
 
