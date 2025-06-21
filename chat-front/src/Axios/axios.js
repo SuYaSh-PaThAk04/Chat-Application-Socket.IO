c717588482bf6641d765db46f0eb79b3ddb85678
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 00:05:47 +0530

    Update axios.js

--- chat-front/src/Axios/axios.js
+++ chat-front/src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:3000/api",
+    baseURL : "https://chat-application-socket-io-r7sq.onrender.com/api",
   withCredentials : true,
 });
