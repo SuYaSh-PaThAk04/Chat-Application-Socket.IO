2d9ae446c800f6cb2d6d1cc92b693154ef473741
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 00:09:20 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chat-application-socket-io-dun.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
