5ca5c72b38fc40cac72531645b8e947ccf11a9ad
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 14:18:55 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,8 @@ import cors from "cors";
 const app = express();
 
 const allowedOrigins = [
-  "https://chat-application-socket-cudt7aoq8-suyash-pathak04s-projects.vercel.app",
-  "https://chat-application-socket-io-git-main-suyash-pathak04s-projects.vercel.app"
+  "https://chat-application-socket-io-git-main-suyash-pathak04s-projects.vercel.app",
+  "https://chat-application-socket-io.vercel.app", // if using
 ];
 
 app.use(cors({
@@ -20,8 +20,9 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"]
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
+app.options("*", cors());
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
