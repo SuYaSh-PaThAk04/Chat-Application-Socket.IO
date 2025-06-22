fc81be4f92c3c87b15d253a0d4c3d8057257a8b4
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 16:10:04 +0530

    Update vite.config.js

--- chat-front/vite.config.js
+++ chat-front/vite.config.js
@@ -4,18 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://chat-application-socket-io-r7sq.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/socket.io': {
-        target: 'https://chat-application-socket-io-r7sq.onrender.com',
-        ws: true,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+      '/api': 'https://chat-application-socket-io-r7sq.onrender.com'
+    }
   },
   plugins: [react()],
 });
+
