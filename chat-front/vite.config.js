7b6630d8726882dbe07fe0918904317ebbfb4053
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 10:20:55 +0530

    Update vite.config.js

--- chat-front/vite.config.js
+++ chat-front/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
      server: {
   proxy: {
-    '/api': 'https://charging-station-backend-o9ky.onrender.com'
+    '/api': 'https://chat-application-socket-io-r7sq.onrender.com'
   }
 },
   plugins: [react()],
