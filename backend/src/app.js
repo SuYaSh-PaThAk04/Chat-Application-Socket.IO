2e1cac73290fe84fb0dc89b2a12b139cfec7216f
Suyash Pathak <suyashp271@gmail.com>
2025-06-22 18:53:03 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,24 +6,13 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = [
-  "https://chat-application-socket-io-git-main-suyash-pathak04s-projects.vercel.app",
-  "https://chat-application-socket-io.vercel.app"
-];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "https://chat-application-socket-io-git-main-suyash-pathak04s-projects.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
