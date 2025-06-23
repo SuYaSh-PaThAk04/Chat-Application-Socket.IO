decb6fc8bb2e30b8bbf02af45ad4898e0a6ebb1a
Suyash Pathak <suyashp271@gmail.com>
2025-06-23 07:59:01 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,9 +27,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
